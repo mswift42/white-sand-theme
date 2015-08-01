37c8275f146011a157510fd6389a2ef58fbc800c
mswift42 <martin.haesler@gmail.com>
2015-08-01 11:27:05 +0100

    Set functionnameface to a35c48.

--- white-sand-theme.el
+++ white-sand-theme.el
@@ -44,7 +44,7 @@
        (keyword "#4a858c")
        (const   "#646496")
        (comment "#a9a9a9")
-       (func    "#71a19f")
+       (func    "#a35c48")
        (str     "#b3534b")
        (type    "#b53875")
        (var     "#476238")
